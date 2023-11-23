d0c84f8873aacef4b0ddb26e36c97708712a99e9
Cem <cemtatli3535@gmail.com>
2023-11-24 00:45:43 +0300

    Add button component and update header design

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,25 @@
 'use client'
 
 import Logo from '@/components/logo'
-import ThemeToggleButton from '@/components/theme-button'
+
 import Navbar from '@/components/navbar'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <header className="flex h-16 w-full items-center justify-between px-8 md:px-20">
       <>
         <div className="flex items-center justify-between">
-          <p className="font-handwriting flex items-center justify-center gap-x-1 font-semibold">
+          <Link
+            href="/"
+            className="font-handwriting flex items-center justify-center gap-x-1 bg-gradient-to-tr font-semibold">
             <Logo size={16} />
             curations
-          </p>
+          </Link>
         </div>
         <Navbar />
-        <ThemeToggleButton />
+        <Button variant="epigra">Add your bookmark</Button>
       </>
     </header>
   )
