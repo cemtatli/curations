eae1ff6433f29cf76840b9bcdd1a5019fd53809e
Cem <cemtatli3535@gmail.com>
2023-11-24 02:29:43 +0300

    Update header height and logo size

--- components/header.tsx
+++ components/header.tsx
@@ -8,13 +8,12 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header className="flex h-16 w-full items-center justify-between px-8 md:px-20">
+    <header className="flex h-20 w-full items-center justify-between px-8 md:px-20">
       <div className="flex items-center justify-between">
         <Link
           href="/"
           className="flex items-center justify-center gap-x-1 bg-gradient-to-tr font-handwriting font-semibold">
-          <Logo size={16} />
-          Curations
+          <Logo size={24} />
         </Link>
       </div>
       <Navbar />
